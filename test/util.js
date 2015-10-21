a4d743ee021fec1c9b2f6457d72a67daad7d0ca4
Luka Vidakovic <luka.vidakovic@gmail.com>
2015-10-21 22:45:19 +0200

    Product tests

--- test/util.js
+++ test/util.js
@@ -5,6 +5,29 @@ var sum = util.sum;
 var product = util.product;
 var createArrayFromArgs = util.createArrayFromArgs;
 
+function mul3(n) { return n * 3; }
+function undef(n) {}
+function nan(n) { return NaN; }
+function zero(n) { return 0; }
+function same(n) { return n; }
+function tricky(n) { return n % 5 === 0 ? false : n; }
+
+var opts = {
+  start: 5,
+  inc: 2,
+  defValue: 14,
+};
+
+var optsNoStart = {
+  inc: 1,
+  defValue: 14,
+};
+
+var optsNoInc = {
+  start: 5,
+  defValue: 14,
+};
+
 describe('util.isPrime', function () {
   it('should return false for non-prime numbers', function () {
     expect(isPrime(-7)).toBe(false);
@@ -26,6 +49,7 @@ describe('util.isPrime', function () {
   });
 });
 
+
 describe('util.findNextPrime', function () {
   it('should return the same number if given prime', function () {
     expect(findNextPrime(3)).toBe(3);
@@ -41,30 +65,8 @@ describe('util.findNextPrime', function () {
   });
 });
 
+
 describe('util.sum', function () {
-  function mul3(n) { return n * 3; }
-  function undef(n) {}
-  function nan(n) { return NaN; }
-  function zero(n) { return 0; }
-  function same(n) { return n; }
-  function tricky(n) { return n % 5 === 0 ? false : n; }
-
-  var opts = {
-    start: 5,
-    inc: 2,
-    defValue: 14,
-  };
-
-  var optsNoStart = {
-    inc: 1,
-    defValue: 14,
-  };
-
-  var optsNoInc = {
-    start: 5,
-    defValue: 14,
-  };
-
   it('should return defValue if start is bigger than end', function () {
     expect(sum(mul3, 2, opts)).toBe(14); // end is 2, start is 5
     expect(sum(mul3, 3, opts)).toBe(14);
@@ -86,7 +88,7 @@ describe('util.sum', function () {
     expect(sum(tricky, 10, opts)).toBe(16);
   });
 
-  it('should return defValue or 0 if sum is 0', function () {
+  it('should return defValue or 0 if final sum is 0', function () {
     expect(sum(undef, 10, opts)).toBe(14);
     expect(sum(nan, 10, opts)).toBe(14);
     expect(sum(zero, 10, opts)).toBe(14);
@@ -97,5 +99,42 @@ describe('util.sum', function () {
     expect(sum(mul3, 13, opts)).toBe(96);
     expect(sum(mul3, 17, opts)).toBe(180);
     expect(sum(mul3, 37, opts)).toBe(960);
+  });
+});
+
+
+describe('util.product', function () {
+  it('should return defValue if start is bigger than end', function () {
+    expect(product(mul3, 2, opts)).toBe(14); // end is 2, start is 5
+    expect(product(mul3, 3, opts)).toBe(14);
+    expect(product(mul3, 4, opts)).toBe(14);
+  });
+
+  it('should start from 0 if start is not defined', function () {
+    expect(product(mul3, 4, optsNoStart)).toBe(162);
+    expect(product(mul3, 5, optsNoStart)).toBe(1944);
+  });
+
+  it('should increment by 1 if inc is not defined', function () {
+    expect(product(mul3, 6, optsNoInc)).toBe(15);
+    expect(product(mul3, 7, optsNoInc)).toBe(270);
+  });
+
+  it('should multiply by 1 when function returns falsy value', function () {
+    expect(product(tricky, 8, opts)).toBe(7);
+    expect(product(tricky, 10, opts)).toBe(63);
+  });
+
+  it('should return defValue or 1 if final product is 1', function () {
+    expect(product(undef, 10, opts)).toBe(14);
+    expect(product(nan, 10, opts)).toBe(14);
+    expect(product(zero, 10, opts)).toBe(14);
+    expect(product(zero, 10)).toBe(1);
+  });
+
+  it('should multiply the values returned by function', function () {
+    expect(product(mul3, 9, opts)).toBe(315);
+    expect(product(mul3, 13, opts)).toBe(280665);
+    expect(product(mul3, 17, opts)).toBe(492567075);
   });
 });
