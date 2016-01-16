7bbfd836fdc3b990cce6112562c986568f240954
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-01-16 12:06:20 +0100

    Expose globs, or commonjs using browserify conf

--- Makefile
+++ Makefile
@@ -6,16 +6,16 @@ BROWSERIFY = $(NODE_MODULES)/.bin/browserify
 # Build production package
 production:
 	@echo "Building steganography.js..."
-	@$(BROWSERIFY) ./src/index.js | $(UGLIFYJS) -mc warnings=false --lint > dist/steganography.js
+	@$(BROWSERIFY) ./src/index.js --standalone steg | $(UGLIFYJS) -mc warnings=false --lint > dist/steganography.js
 	@echo "Build finished."
 
 # Build production package
 development:
 	@echo "Building steganography.js..."
-	@$(BROWSERIFY) -d ./src/index.js -o dist/steganography.js
+	@$(BROWSERIFY) -d ./src/index.js -o dist/steganography.js --standalone steg
 	@echo "Build finished."
 
 # Watch src files and rebuild bundle while developing
 watch:
 	@echo "Watching nextuser source files..."
-	@$(WATCHIFY) ./src/index.js -o dist/steganography.js -v
+	@$(WATCHIFY) ./src/index.js -o dist/steganography.js --standalone steg -v
--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 /*
  * SteganOgre.js
  *
- * Based on Steganography.js by Peter Eigenschink (http://www.peter-eigenschink.at/)
- * Dual-licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and the Beerware (http://en.wikipedia.org/wiki/Beerware) license.
+ * almost completely rewritten Steganography.js library by Peter Eigenschink (http://www.peter-eigenschink.at/)
+ * to be more modular, testable and usable throuh npm
+ *
  */
 
 var encode = require('./encode');
 var decode = require('./decode');
 var getHidingCapacity = require('./getHidingCapacity');
 
-function initLibrary() {
-  return {
-    encode: encode,
-    decode: decode,
-    getHidingCapacity: getHidingCapacity,
-  };
-}
-
-// expose for commonjs, amd and globs
-(function(name, definition) {
-    if (typeof module != 'undefined') module.exports = definition();
-    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
-    else this[name] = definition();
-}('steganogre', initLibrary));
-
-// TODO: remove this code; only used while hackking together inital version
-window.steg = initLibrary();
+module.exports = {
+  encode: encode,
+  decode: decode,
+  getHidingCapacity: getHidingCapacity,
+};
