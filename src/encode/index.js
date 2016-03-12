065e5365a0c318a5f2da2a88f3333c6a6ae24bb3
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-03-12 20:14:19 +0100

    refactor

--- src/encode/index.js
+++ src/encode/index.js
@@ -1,7 +1,12 @@
+import requiredPixels from '../util/required-pixels'
+import createShadowCanvas from '../util/create-shadow-canvas'
 import chunkMessage from './chunk-message'
 
-export default function encode(canvas, msg) {
+export default function encode(msg) {
+  const pixelNum = requiredPixels(msg)
+  const canvas = createShadowCanvas(pixelNum, 1) // TODO: options for this?
   const chunks = chunkMessage(msg)
+  canvas.putData(chunks)
 
-  return canvas.putData(chunks)
+  return canvas
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import createShadowCanvas from './util/create-shadow-canvas'
 import encode from './encode/'
-import requiredPixels from './util/required-pixels'
 
 const msg = 'abcdefg'
-const pixelNum = requiredPixels(msg)
+const canvas = encode(msg)
 
-const canvas = createShadowCanvas(pixelNum, 1)
+console.log(canvas.getData())
 
 export default {
-  createShadowCanvas,
   encode,
-  requiredPixels,
 }
