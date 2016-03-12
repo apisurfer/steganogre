3f3ae65137af84cac761e7df3026154f93413964
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-03-13 00:00:07 +0100

    cleanup

--- src/decode/index.js
+++ src/decode/index.js
@@ -5,6 +5,7 @@ import extractMessage from './extract-message'
 export default function decode(imageURL) {
   return new Promise((resolve, reject) => {
     const img = new Image();
+    img.crossOrigin = 'Anonymous' // enable cross origin content
 
     img.onload = () => {
       const wrappedCanvas = wrapCanvas(
