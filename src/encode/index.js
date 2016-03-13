870f5dda8efa39c530a230f2dec8107761f8aa16
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-03-13 18:02:57 +0100

    use high-factors to calculate img dimensions

--- src/encode/index.js
+++ src/encode/index.js
@@ -1,3 +1,4 @@
+import hf from 'high-factors'
 import requiredPixels from '../util/required-pixels'
 import createShadowCanvas from '../util/create-shadow-canvas'
 import wrapCanvas from '../util/wrap-canvas'
@@ -5,12 +6,13 @@ import chunkMessage from './chunk-message'
 
 export default function encode(msg) {
   const pixelNum = requiredPixels(msg)
-  const canvas = createShadowCanvas(pixelNum, 1) // TODO: options for sizing?
+  const imgDimensions = hf(pixelNum)
+  const canvas = createShadowCanvas(imgDimensions[0], imgDimensions[1])
   const wrappedCanvas = wrapCanvas(canvas)
   const chunks = chunkMessage(msg)
   wrappedCanvas.putData(chunks)
 
-  const dataURL = wrappedCanvas.el.toDataURL('image/png');
+  const dataURL = wrappedCanvas.el.toDataURL('image/png')
   wrappedCanvas.el.width = 0
   wrappedCanvas.el.heigh = 0
 
