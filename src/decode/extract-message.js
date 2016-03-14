6a1b5fa26fb6eeee727ba6bbaea886450b5323d2
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-03-14 22:59:51 +0100

    bug fix for extracting data

--- src/decode/extract-message.js
+++ src/decode/extract-message.js
@@ -3,23 +3,24 @@ export default function extractMessage(imageData) {
   const msgChunks = []
   let msg = ''
 
-  console.clear()
-
   for (let i = 0; i < imageData.length; i += 4) {
-    console.log(imageData[i])
-    console.log(imageData[i + 1])
-    console.log(imageData[i + 2])
     msgChunks.push(
       imageData[i],
       imageData[i + 1],
       imageData[i + 2]
     )
   }
-  console.log(msgChunks)
 
   // no need for "leftover" byte; utf-16 uses 2 bytes so
   // when we have a hanging one from RGB components we discard it
   if (msgChunks.length % 2 !== 0) {
+    msgChunks.pop()
+  }
+
+  // if last two bytes are empty just drop them; remaining because 2byte chars are stored using
+  // rgb(3 bytes) channels per pixel;
+  if (msgChunks[msgChunks.length -1] === 0 && msgChunks[msgChunks.length -2] === 0) {
+    msgChunks.pop()
     msgChunks.pop()
   }
 
