2825deacdd17d481792e8f56c47a8194090e37e8
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-03-17 01:09:36 +0100

    height defaults to width value

--- src/encode/calculate-dimension.js
+++ src/encode/calculate-dimension.js
@@ -1,7 +1,7 @@
 export default function calculateDimension(num) {
   const width = Math.floor(Math.sqrt(num))
   const remainder = num - (width * width)
-  let height
+  let height = width
 
   if (remainder) {
     height = remainder >= width ? width + Math.ceil(remainder / width) : width + 1
