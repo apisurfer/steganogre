5fad0a5d4cc3983c1659504cc570ebea4a4d928d
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-04-13 13:50:41 +0200

    cli encode

--- dist/cli.js
+++ dist/cli.js
@@ -1,28 +1,51 @@
 var fs = require('fs')
+var path = require('path')
 var Canvas = require('canvas')
 var minimist = require('minimist')
 var encode = require('./steganogre.min.js').encode
 
-var canvas = new Canvas(1, 1)
 var args = minimist(process.argv.slice(2))
-var msg = args._[0]
-var filename = args._[1]
+var msg = args.m
+var first = args._[0]
+var second = args._[1]
 
-if (!msg || !filename) {
-  console.error('You must provide message and filename parameters')
+if (!msg && (!first || !second)) {
+  console.log('Incorrect parameters passed!')
 
   return
 }
 
-var wraped = encode(msg, canvas)
-var out = fs.createWriteStream(__dirname + '/' + filename)
-var stream = canvas.pngStream()
+if (msg) {
+  if (!first) {
+    console.error('You need to specify output name!')
 
-stream.on('data', function(chunk){
-  out.write(chunk);
-});
+    return
+  }
 
-stream.on('end', function(){
-  console.log('msg encoded to ' + filename);
-});
+  writeMsgToFile(msg, first)
+} else {
+  if (!first || !second) {
+    console.error('You must provide input and output filename!')
 
+    return
+  }
+
+  var content = fs.readFileSync(first, { encoding: 'utf8' })
+
+  writeMsgToFile(content, second)
+}
+
+function writeMsgToFile(msg, filename) {
+  var canvas = new Canvas(1, 1)
+  var wraped = encode(msg, canvas)
+  var out = fs.createWriteStream(path.join(__dirname + '/' + filename))
+  var stream = canvas.pngStream()
+
+  stream.on('data', function(chunk){
+    out.write(chunk);
+  });
+
+  stream.on('end', function(){
+    console.log('msg encoded to ' + filename);
+  });
+}
