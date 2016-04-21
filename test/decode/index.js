b16ef908f1cca469b443d751344c3181afb07fb2
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-04-21 23:23:51 +0200

    repair for encode & deocde tests

--- test/decode/index.js
+++ test/decode/index.js
@@ -1,12 +1,17 @@
 import 'babel-polyfill'
 import decode from '../../src/decode/index'
 
-const imgURI = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAHUlEQVQIHQESAO3/AABmAP9vAG//AgD8AADyAAMAM4YFNhwjbZQAAAAASUVORK5CYII='
+const imgURI = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAGklEQVQIW2NkSGP4nz8rn4GRIYnhf+K8IgYAMgoFsr/d3psAAAAASUVORK5CYII='
 
 describe('decode', () => {
   it('should return Promise', () => {
     expect(decode(imgURI) instanceof Promise).toBe(true)
   })
 
-  // TODO: test response
+  it('should return correct value', (done) => {
+    decode(imgURI).then(data => {
+      expect(data).toBe('foobar')
+      done()
+    })
+  })
 })
--- test/encode/index.js
+++ test/encode/index.js
@@ -1,10 +1,15 @@
 import encode from '../../src/encode/index'
 
 describe('encode', () => {
-  it('should return the correct dataURL & download url', () => {
-    const expectedURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAHUlEQVQIHQESAO3/AABmAP9vAG//AgD8AADyAAMAM4YFNhwjbZQAAAAASUVORK5CYII=';
+  it('should return the correct dataURL', () => {
+    const expectedURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAGklEQVQIW2NkSGP4nz8rn4GRIYnhf+K8IgYAMgoFsr/d3psAAAAASUVORK5CYII=';
 
-    expect(encode('foobar').dataURL).toBe(expectedURL)
-    expect(encode('foobar').downloadHref()).toBe(expectedURL.replace('image/png', 'image/octet-stream'))
+    expect(encode('foobar').dataURL).toEqual(expectedURL)
+  })
+
+  it('should return the correct download url', () => {
+    const expectedURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAGklEQVQIW2NkSGP4nz8rn4GRIYnhf+K8IgYAMgoFsr/d3psAAAAASUVORK5CYII=';
+
+    expect(encode('foobar').downloadHref()).toEqual(expectedURL.replace('image/png', 'image/octet-stream'))
   })
 })
