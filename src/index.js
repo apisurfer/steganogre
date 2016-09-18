3d8ed82acbdb13b5dc02f1260743b4ba5d0fbbfe
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-09-18 19:23:08 +0200

    verify strategy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function verifyStrategy (strategy) {
+  if (!strategy) throw Error('No strategy provided!')
+
+  if (!strategy.encode || typeof strategy.encode !== 'function') throw Error('Strategy lacks encode method!')
+  if (!strategy.decode || typeof strategy.decode !== 'function') throw Error('Strategy lacks decode method!')
+  if (!strategy.calculateSize || typeof strategy.calculateSize !== 'function') throw Error('Strategy lacks calculateSize method!')
+}
+
 export default function steganogre (strategy) {
-  throw Error('No strategy provided!')
+  verifyStrategy(strategy)
 }
--- test/index.js
+++ test/index.js
@@ -4,4 +4,22 @@ describe('steganogre module initialization', () => {
   it('should throw if not provided with strategy', () => {
     expect(s).toThrowError(/no strategy provided!/i)
   })
+
+  it('should throw if bad strategy doesn\'t have a encode function', () => {
+    const strategyMock = { decode() {}, calculateSize() {} }
+
+    expect(() => s(strategyMock)).toThrowError(/Strategy lacks encode method!/i)
+  })
+
+  it('should throw if bad strategy doesn\'t have a decode function', () => {
+    const strategyMock = { encode() {}, calculateSize() {} }
+
+    expect(() => s(strategyMock)).toThrowError(/Strategy lacks decode method!/i)
+  })
+
+  it('should throw if bad strategy doesn\'t have a de function', () => {
+    const strategyMock = { encode() {}, decode() {} }
+
+    expect(() => s(strategyMock)).toThrowError(/Strategy lacks calculateSize method!/i)
+  })
 })
