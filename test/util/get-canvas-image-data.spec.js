4d4e0f2a0517e51ca9e42c40af7f4ef724f46556
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-09-18 22:05:08 +0200

    additional test for file type

--- test/util/get-canvas-image-data.spec.js
+++ test/util/get-canvas-image-data.spec.js
@@ -2,10 +2,13 @@ import getCanvasImageData from '../../src/util/get-canvas-image-data'
 import createCanvas from '../../src/util/create-canvas'
 
 describe('getCanvasImageData', () => {
-  it('should return canvas image data in form of an array', () => {
+  it('should return canvas image data in form of an Uint8ClampedArray', () => {
     const canvas = createCanvas()
+    const canvasImageData = getCanvasImageData(canvas)
 
-    expect(getCanvasImageData(canvas).length)
+    expect(canvasImageData instanceof Uint8ClampedArray).toBe(true)
+
+    expect(canvasImageData.length)
       .toBe(canvas.width * canvas.height * 4)
   })
 })
